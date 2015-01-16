2acb7d2e496cf5cbf9d6159a5e363891fde1da5c
gsusmobile <gsnagel@gmail.com>
2015-01-15 21:47:30 -0500

    Works, need to ensure service starts after elasticsearch class declaration.

--- puppet/manifest.pp
+++ puppet/manifest.pp
@@ -19,25 +19,13 @@ class { 'mongodb::server':
   bind_ip => ["0.0.0.0"],
 }
 
-# See https://github.com/elasticsearch/puppet-elasticsearch/issues/39
-class our_elasticsearch($version='1.4.2') {
 
-  # We couldn't simply rely on the 'elasticsearch' module from:
-  # https://github.com/elasticsearch/puppet-elasticsearch because it requires your desired
-  # version to be in your package manager, but ours isn't.
-
-  exec { "download_elasticsearch":
-    command => "/usr/bin/wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-${version}.deb -O /tmp/elasticsearch-${version}.deb",
-    creates => "/tmp/elasticsearch-${version}.deb"
-  }
-
-  package { 'openjdk-7-jre': ensure => present }
-
-  class { 'elasticsearch':
-    package_url => "file:/tmp/elasticsearch-${version}.deb",
-    require     => [ Package['openjdk-7-jre'], Exec['download_elasticsearch'] ],
-    autoupgrade => true,
-  }
+class { 'elasticsearch':
+  package_url => "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.2.deb",
+  java_install => true,
 }
 
-include our_elasticsearch
+exec {
+  "start elasticsearch":
+  command => "/usr/share/elasticsearch/bin/elasticsearch -d"
+}
