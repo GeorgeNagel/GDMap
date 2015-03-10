d2891cc8720ece2a69b483cdf4b8df0aa5fea604
gsusmobile <gsnagel@gmail.com>
2015-03-09 21:01:59 -0400

    Find locations with mongo => better

--- scripts/find_locations.py
+++ scripts/find_locations.py
@@ -1,30 +1,17 @@
 """Generate a list of locations from the index."""
 
-from gdmap.es_index import query_es
+from mongoengine import connect
 
-locations_query_body = {
-    'query': {
-        # Return the locations for all documents
-        'match_all': {}
-    },
-    'aggregations': {
-        'locations_aggregation': {
-            'terms': {
-                'field': 'location',
-                # Return all location buckets
-                'size': 0
-            }
-        }
-    },
-    # Don't return query results, just the aggregation
-    'size': 0
-}
+from gdmap.models import Song
+from gdmap.settings import MONGO_DATABASE_NAME
+
+
+# Establish the connection to the database
+connect(MONGO_DATABASE_NAME)
 
 
 def find_locations():
-    query_result = query_es(locations_query_body)
-    location_buckets = query_result['aggregations']['locations_aggregation']['buckets']
-    locations = [bucket['key'] for bucket in location_buckets]
+    locations = [loc for loc in Song.objects.all().distinct('location')]
     return locations
 
 
