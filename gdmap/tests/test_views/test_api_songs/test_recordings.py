47416f409e97a964abcef7d4689767bdcee92518
gsusmobile <gsnagel@gmail.com>
2015-03-16 21:17:25 -0400

    Fix recording api show_id filtering bug.

--- gdmap/tests/test_views/test_api_songs/test_recordings.py
+++ gdmap/tests/test_views/test_api_songs/test_recordings.py
@@ -9,7 +9,7 @@ from gdmap.tests.utils import mongo_clean, APITestCase
 log = logging.getLogger(__name__)
 
 
-class SongsAPITestCase(APITestCase):
+class RecordingsAPITestCase(APITestCase):
     def setUp(self):
         # Two songs from the same concert, different recordings
         self.test_song_1 = Song(sha1='abc123',
@@ -19,6 +19,7 @@ class SongsAPITestCase(APITestCase):
                                 title='test_title_1',
                                 track=1,
                                 date='1980-01-02',
+                                latlon='1,-2',
                                 location='New York, NY')
 
         self.test_song_2 = Song(sha1='abc1234',
@@ -28,6 +29,7 @@ class SongsAPITestCase(APITestCase):
                                 title='test_title_2',
                                 track=2,
                                 date='1980-01-02',
+                                latlon='1,-2',
                                 location='New York, NY')
 
         # A song from another concert
@@ -38,8 +40,9 @@ class SongsAPITestCase(APITestCase):
                                 title='test_title_3',
                                 track=2,
                                 date='1990-01-01',
+                                latlon='2,3',
                                 location='Bingo, NY')
-        super(SongsAPITestCase, self).setUp()
+        super(RecordingsAPITestCase, self).setUp()
 
     @mongo_clean
     def test_query_recordings(self):
@@ -59,29 +62,64 @@ class SongsAPITestCase(APITestCase):
         self.assertEqual(
             json.loads(response.data),
             {
-                u'recordings': [
+                'recordings': [
                     {
-                        u'album': u'test album 2',
-                        u'date': u'1990-01-01',
-                        u'location': u'Bingo, NY',
-                        u'show_id': u'test_show_id_3',
-                        u'total': 1
+                        'album': 'test album 2',
+                        'date': '1990-01-01',
+                        'latlon': '2,3',
+                        'location': 'Bingo, NY',
+                        'show_id': 'test_show_id_3',
+                        'total': 1
                     },
                     {
-                        u'album': u'test album 1',
-                        u'date': u'1980-01-02',
-                        u'location': u'New York, NY',
-                        u'show_id': u'test_show_id_1',
-                        u'total': 1
+                        'album': 'test album 1',
+                        'date': '1980-01-02',
+                        'latlon': '1,-2',
+                        'location': 'New York, NY',
+                        'show_id': 'test_show_id_1',
+                        'total': 1
                     },
                     {
-                        u'album': u'test album 1',
-                        u'date': u'1980-01-02',
-                        u'location': u'New York, NY',
-                        u'show_id': u'test_show_id_2',
-                        u'total': 1
+                        'album': 'test album 1',
+                        'date': '1980-01-02',
+                        'latlon': '1,-2',
+                        'location': 'New York, NY',
+                        'show_id': 'test_show_id_2',
+                        'total': 1
                     }
                 ],
-                u'total': 3
+                'total': 3
+            }
+        )
+
+    @mongo_clean
+    def test_query_recording_id(self):
+        """Test querying and filtering on a recording id."""
+        self.maxDiff = None
+        log.debug("Saving songs in Mongo.")
+        self.test_song_1.save()
+        self.test_song_2.save()
+        self.test_song_3.save()
+        self.assertEqual(Song.objects.count(), 3)
+        log.debug("Indexing test song.")
+        index_songs()
+        # Wait for the song to be indexed
+        time.sleep(2)
+        log.debug("Getting all recordings.")
+        response = self.app.get('/api/recordings/?show_id=test_show_id_2')
+        self.assertEqual(
+            json.loads(response.data),
+            {
+                'recordings': [
+                    {
+                        'album': 'test album 1',
+                        'date': '1980-01-02',
+                        'latlon': '1,-2',
+                        'location': 'New York, NY',
+                        'show_id': 'test_show_id_2',
+                        'total': 1
+                    }
+                ],
+                'total': 1
             }
         )
